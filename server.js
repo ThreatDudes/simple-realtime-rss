abb7fdf82e086c7b1464ce4e181ec1dab5101393
KrautByte <lars@krautbyte.com>
2022-01-27 08:55:08 +0100

    Added error logging with winston into different file, now in json format for easier usage with monitoring later on...

--- server.js
+++ server.js
@@ -2,10 +2,29 @@
 const config = require('./config.json')
 const feeds = require('./feeds.json')
 const redis = require('redis')
+const winston = require('winston')
 
 // external imports
 const RssFeedEmitter = require('rss-feed-emitter')
 const request = require('request')
+
+const logger = winston.createLogger({
+    level: 'info',
+    format: winston.format.json(),
+    transports: [
+        //
+        // - Write all logs with importance level of `error` or less to `error.log`
+        // - Write all logs with importance level of `info` or less to `combined.log`
+        //
+        new winston.transports.File({ filename: 'error.log', level: 'error' }),
+        new winston.transports.File({ filename: 'combined.log' }),
+    ],
+});
+
+/* Fix bug wiht that one (broken by design more like 'we cant find that bug and need a cheap excuse') */
+const errorHelper = {
+    error: logger.error.bind(logger)
+};
 
 // initialize redis
 const redisClient = redis.createClient({
@@ -18,7 +37,7 @@ redisClient.on('error', err => {
 
 // array of urls from sources.json where active is true
 const urls = feeds.filter(entry => {
-    if (entry.active == true) {
+    if (entry.active) {
         return entry
     }
 }).map(entry => (entry.url))
@@ -37,7 +56,7 @@ urls.forEach((url) => {
 
 // instructions on what to do when new item from feeds is pushed
 
-feeder.on('new-item', function (item) {
+feeder.on('new-item', async function (item) {
     try {
         const timestamp = item.pubdate
         const link = item.link
@@ -61,12 +80,11 @@ feeder.on('new-item', function (item) {
         }
         const itemObject = {timestamp, link, title, author, text_summary}
 
-        /*
-        redisClient.get(itemObject.link, (err, reply) => {
-            if(err) console.log(err);
-            if (!reply) return false;
+        await redisClient.get(itemObject.link, (err, reply) => {
+            if (err) errorHelper.error(err);
+            errorHelper.error(`redis reply ${reply}`)
         })
-         */
+
 
         var message_string = `**${title}**\nPublished: ${timestamp}\nby ${author}\n${link}\n`
 
@@ -81,14 +99,14 @@ feeder.on('new-item', function (item) {
 
         const req = request.post(options, (err, res, body) => {
             if (err) {
-                return console.log(`[+] ERR posting "${title}": ${err}`);
+                return errorHelper.error(`[+] ERR posting "${title}": ${err}`);
             }
             console.log(`[+] Posted "${title}", status ${res.statusCode}`);
         });
 
     } catch (err) {
-        console.log(`[!] ERR: ${err}`)
+        errorHelper.error(`[!] ERR: ${err}`)
     }
 })
 
-feeder.on('error', console.error);
+feeder.on('error', errorHelper.error);
