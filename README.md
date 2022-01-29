420d947de66c00c594c87e2010f01702780c9fc8
KrautByte <lars@krautbyte.com>
2022-01-29 16:50:06 +0100

    added the possiblity of having multiply webhooks, removed redis support since I am not even sure anymore if its really needed, not sure if it would even figure.
    Signed-off-by: KrautByte <lars@krautbyte.com>

--- README.md
+++ README.md
@@ -1,16 +1,20 @@
 # simple-realtime-rss
+
 The simplest realtime RSS feed server. No bot Oauth or anything!
 
-
 ## Installing
-For RSS, we use the external library here [`rss-feed-emitter`](https://www.npmjs.com/package/rss-feed-emitter). It can be installed with
+
+For RSS, we use the external library here [`rss-feed-emitter`](https://www.npmjs.com/package/rss-feed-emitter). It can
+be installed with
+
 ```
 npm i rss-feed-emitter
 ```
-Redis is also used as databse for storing feed information. [Read this quickstart here](https://redis.io/topics/quickstart) for instructions on installation. In the `config.json` make sure your Redis host IP and Port match what is running on the system.
 
 ## Configuration
+
 In `feeds.json` you can list out the feeds you would want to ingest with this schema:
+
 ```
 {
       "id": "e5c320e2-b058-415a-9b55-2c3230c6b59e",
@@ -24,9 +28,12 @@ In `feeds.json` you can list out the feeds you would want to ingest with this sc
       "description": "The Exploit Database - Exploits, Shellcode, 0days, Remote Exploits, Local Exploits, Web Apps, Vulnerability Reports, Security Articles, Tutorials and more."
 }
 ```
-The only required fields in this schema are `url` and `active`. The server will not ingest any feeds that are not `"active": true`.
+
+The only required fields in this schema are `url` and `active`. The server will not ingest any feeds that are
+not `"active": true`.
 
 In `config.json`, you may set the refresh interval and the webhook url:
+
 ```
 {
     "webhook": "<url>",
@@ -34,7 +41,12 @@ In `config.json`, you may set the refresh interval and the webhook url:
 }
 ```
 
-For instance, a Discord channel webhook can be obtained by following these instructions: [https://support.discord.com/hc/en-us/articles/228383668-Intro-to-Webhooks](https://support.discord.com/hc/en-us/articles/228383668-Intro-to-Webhooks).
+For instance, a Discord channel webhook can be obtained by following these
+instructions: [https://support.discord.com/hc/en-us/articles/228383668-Intro-to-Webhooks](https://support.discord.com/hc/en-us/articles/228383668-Intro-to-Webhooks)
+.
 
 ## Running
-Just run `node server.js`. An initial poll will grab all the current content, then it will just run into perpetuity. You can deploy to your server as a background task using `nohup node server.js &` or even maybe using [Glitch](https://glitch.com) as free hosting.
+
+Just run `node server.js`. An initial poll will grab all the current content, then it will just run into perpetuity. You
+can deploy to your server as a background task using `nohup node server.js &` or even maybe
+using [Glitch](https://glitch.com) as free hosting.
--- server.js
+++ server.js
@@ -1,60 +1,47 @@
-// local imports
+// Local imports
 const config = require('./config.json')
 const feeds = require('./feeds.json')
-const redis = require('redis')
 const winston = require('winston')
 
-// external imports
+// External imports
 const RssFeedEmitter = require('rss-feed-emitter')
 const request = require('request')
 
 const logger = winston.createLogger({
-    level: 'info',
-    format: winston.format.json(),
-    transports: [
-        //
+    level: 'info', format: winston.format.json(), transports: [//
         // - Write all logs with importance level of `error` or less to `error.log`
         // - Write all logs with importance level of `info` or less to `combined.log`
         //
-        new winston.transports.File({ filename: 'error.log', level: 'error' }),
-        new winston.transports.File({ filename: 'combined.log' }),
-    ],
+        new winston.transports.File({
+            filename: 'error.log', level: 'error'
+        }), new winston.transports.File({filename: 'combined.log'}),],
 });
 
-/* Fix bug wiht that one (broken by design more like 'we cant find that bug and need a cheap excuse') */
+// Bugfix for Winston Mess-Up (Broken by design more like "We can't find that bug and need a cheap excuse")
 const errorHelper = {
     error: logger.error.bind(logger)
 };
 
-// initialize redis
-const redisClient = redis.createClient({
-    host: config.redis_host, port: config.redis_port, password: config.redis_password
-})
-redisClient.connect();
-redisClient.on('error', err => {
-    console.log('Error ' + err);
-});
-
-// array of urls from sources.json where active is true
+// Array of urls from sources.json where active is true
 const urls = feeds.filter(entry => {
     if (entry.active) {
         return entry
     }
 }).map(entry => (entry.url))
 
-// initialize the feed object, with options
+// Initialize the feed object, with options
 const feeder = new RssFeedEmitter({
-    skipFirstLoad: true // avoids the dump of current-state data from feeds
+    skipFirstLoad: true // Avoids the dump of current-state data from feeds
 });
 
-// add our urls with the refersh interval from config.json
+// Add our urls with the refresh interval from config.json
 urls.forEach((url) => {
     feeder.add({
         'url': url, 'refresh': config.refresh_interval
     });
 });
 
-// instructions on what to do when new item from feeds is pushed
+// Instructions on what to do when new item from feeds is pushed
 
 feeder.on('new-item', async function (item) {
     try {
@@ -64,7 +51,7 @@ feeder.on('new-item', async function (item) {
         const author = item.author
         const summary = item.summary
         const description = item.description
-        var text_summary = ''
+        let text_summary = '';
         try {
             text_summary = summary.replace(/<[^>]*>/g, '').replace('\n', ' ')
         } catch (err) {
@@ -80,33 +67,31 @@ feeder.on('new-item', async function (item) {
         }
         const itemObject = {timestamp, link, title, author, text_summary}
 
-        await redisClient.get(itemObject.link, (err, reply) => {
-            if (err) errorHelper.error(err);
-            errorHelper.error(`redis reply ${reply}`)
-        })
-
-
-        var message_string = `**${title}**\nPublished: ${timestamp}\nby ${author}\n${link}\n`
+        let message_string = `**${title}**\nPublished: ${timestamp}\nby ${author}\n${link}\n`;
 
         console.log(`[+] NEW: "${title}"`)
         console.log(`[+] Posting to webhook...`)
 
-        const options = {
-            url: config.webhook, headers: {
-                'Content-Type': 'application/json'
-            }, json: true, body: {'content': message_string}
-        }
-
-        const req = request.post(options, (err, res, body) => {
-            if (err) {
-                return errorHelper.error(`[+] ERR posting "${title}": ${err}`);
+        config.webhook.forEach((webhook) => {
+            const options = {
+                url: webhook, headers: {
+                    'Content-Type': 'application/json'
+                }, json: true, body: {'content': message_string}
             }
-            console.log(`[+] Posted "${title}", status ${res.statusCode}`);
+
+            const req = request.post(options, (err, res, body) => {
+                if (err) {
+                    return errorHelper.error(`[+] ERR posting "${title}": ${err}`);
+                }
+                console.log(`[+] Posted "${title}", status ${res.statusCode}`);
+            });
+
         });
 
     } catch (err) {
         errorHelper.error(`[!] ERR: ${err}`)
     }
+
 })
 
 feeder.on('error', errorHelper.error);
